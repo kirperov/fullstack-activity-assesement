80d69cc1b03880896b0bbfe1c9ae27b2545fa0f0
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2022-02-23 12:43:07 +0100

    add post route

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
-
+app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-
-app.use('/api/products', (req, res, next) => {
+  app.post('/api/products', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        _id: 'oeihfzeoi',
+        name: "hello product",
+        description: "test",
+        price: "150",
+        inStock: true
+    });
+});
+app.get('/api/products', (req, res, next) => {
     const products = [
       {
         _id: 'oeihfzeoi',
@@ -28,12 +37,7 @@ app.use('/api/products', (req, res, next) => {
     res.status(200).json(products);
   });
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+
 
 
 module.exports = app;
