70ede34b049d42fceb9452d86b5c1a896bd06aeb
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2022-02-23 17:35:44 +0100

    Ok get all products, get one, update product and delete product

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
   app.post('/api/products', (req, res, next) => {
-    delete req.body._id;
+   
     const product = new Product({
         ...req.body
     });
@@ -28,8 +29,29 @@ app.use((req, res, next) => {
       .catch(error => res.status(400).json({ error }));
 });
 
+app.get('/api/products', (req, res, next) => {
+  Product.find()
+  .then(products => res.status(200).json({products}))
+  .catch(error => res.status(400).json({ error }));
+});
 
+app.get('/api/products/:id', (req, res, next) => {
+  Product.findOne({ _id: req.params.id })
+    .then(product => res.status(200).json({product}))
+    .catch(error => res.status(404).json({ error }));
+});
 
+app.put('/api/products/:id', (req, res, next) => {
+  // req.params.id c'est l'id envoyé en parametre de la requete,  ...req.body, _id: req.params.id le nouvel objet avec un id en question
+  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Modified!'}))
+    .catch(error => res.status(400).json({ error }));
+});
 
-
+app.delete('/api/products/:id', (req, res, next) => {
+  Product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Deleted!'}))
+    .catch(error => res.status(400).json({ error }));
+});
+  
 module.exports = app;
